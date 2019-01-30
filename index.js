7ecca8e4b9c9a079451b984770f4a5468244a242
Aniket Patel <aniketupatel1@gmail.com>
2019-01-30 02:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 function iLoveTheBeatles(num) {
    var array = [];
     do {
-      array.push("iLoveTheBeatles!")
+      array.push("I love the Beatles!")
       num++;
     } while (num<15);
   return array;
